809e7addade1e8532278a9c6201ff4f06f004da1
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-06-23 21:05:00 +0300

    Include only not null values in eventDto

--- src/main/java/by/events/backend/model/dto/EventDto.java
+++ src/main/java/by/events/backend/model/dto/EventDto.java
@@ -2,11 +2,13 @@ package by.events.backend.model.dto;
 
 import by.events.backend.model.base.BaseDto;
 import by.events.backend.model.entity.Event;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EventDto extends BaseDto {
 
     private long id;
