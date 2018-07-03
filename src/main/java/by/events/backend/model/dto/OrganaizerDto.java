642738e04d4a10d3e14403443fad255430e73aef
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-07-03 13:46:06 +0300

    Changed Organaizer DTO

--- src/main/java/by/events/backend/model/dto/OrganaizerDto.java
+++ src/main/java/by/events/backend/model/dto/OrganaizerDto.java
@@ -2,29 +2,87 @@ package by.events.backend.model.dto;
 
 import by.events.backend.model.base.BaseDto;
 import by.events.backend.model.entity.Organaizer;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrganaizerDto extends BaseDto {
 
-    @JsonProperty("user_data")
-    private UserDto userDto;
+    private long id;
+    private String email;
+    private String username;
+    private String fullname;
+    private String company;
+    private String website;
+    private String phone;
 
     public OrganaizerDto() {
     }
 
     public OrganaizerDto(UserDto userDto) {
-        this.userDto = userDto;
+        this.id = userDto.getId();
+        this.email = userDto.getEmail();
+        this.username = userDto.getUsername();
+        this.fullname = userDto.getFullname();
+        this.company = userDto.getCompany();
+        this.website = userDto.getWebsite();
+        this.phone = userDto.getPhone();
     }
 
-    public UserDto getUserDto() {
-        return userDto;
+    public long getId() {
+        return id;
     }
 
-    public void setUserDto(UserDto userDto) {
-        this.userDto = userDto;
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public static OrganaizerDto toDto(Organaizer organaizer) {
